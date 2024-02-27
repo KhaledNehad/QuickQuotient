a02e236402aa78bb4a5cc16a98c6bf7d070b9e5a
Khaled Nehad <khalednehad@gmail.com>
2024-02-27 13:19:31 +0100

    test Auth callback

--- app/auth/components/OAuthForm.tsx
+++ app/auth/components/OAuthForm.tsx
@@ -9,26 +9,14 @@ export default function Page() {
   const params = useSearchParams();
   const next = params.get("next");
 
-  const getURL = () => {
-    let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      "http://localhost:3000/";
-    // Make sure to include `https://` when not localhost.
-    url = url.includes("http") ? url : `https://${url}`;
-    // Make sure to include a trailing `/`.
-    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-    return url;
-  };
-
   const handleLoginWithOAuth = (provider: "github" | "google") => {
     const supabase = supabaseBrowser();
     supabase.auth.signInWithOAuth({
       provider,
       options: {
         redirectTo: next
-          ? `${getURL()}auth/callback?next=${next}`
-          : `${getURL()}auth/callback`,
+          ? `${location.origin}/auth/callback?next=${next}`
+          : `${location.origin}/auth/callback`,
       },
     });
   };
